7efc4856ed0d33ec68f7fdacb046f7871d3d100f
Yanis Bouallouche <blcyanis@gmail.com>
2023-03-08 16:31:37 +0100

    Récupéré sub et obj à partir de prop

--- Outils-Integration/parseRdf.py
+++ Outils-Integration/parseRdf.py
@@ -48,26 +48,78 @@ def getAllProperty():
     return commonProprety
 
 
-def getSubObjSource(property,graph):
-    print(property[len(property)-1])
-    req = """
-        PREFIX mus: <http://data.doremus.org/ontology#>
-        PREFIX ecrm: <http://erlangen-crm.org/current/>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX efrbroo: <http://erlangen-crm.org/efrbroo/>
-        PREFIX dcterms: <http://purl.org/dc/terms/>
-        
-        SELECT ?resource ?object
-        WHERE{
-            ?resource mus:%s ?object.
-        }
-    """
+def getSubObjSource(property, graph):
 
-    req = req % property
+    namespace=None
+    uriProperty=None
+    list=[]
+
+    if(property.find("#")==-1):
+        list=property.split("/")
+        namespace=list[len(list)-1]
+        list.remove(namespace)
+        uriProperty="/".join(list) + "/"
+    else:
+        list = property.split("#")
+        namespace = list[len(list) - 1]
+        list.remove(namespace)
+        uriProperty = "#".join(list) + "#"
+
+    print(uriProperty)
+
+    req=None
+    if(uriProperty=="http://data.doremus.org/ontology#"):
+        req = """
+            PREFIX mus: <http://data.doremus.org/ontology#>
+    
+            SELECT ?resource ?object
+            WHERE{
+                ?resource mus:%s ?object.
+            }
+        """
+    elif(uriProperty=="http://erlangen-crm.org/current/"):
+        req = """
+            PREFIX ecrm: <http://erlangen-crm.org/current/>
+    
+            SELECT ?resource ?object
+            WHERE{
+                ?resource ecrm:%s ?object.
+            }
+        """
+    elif(uriProperty=="http://www.w3.org/2001/XMLSchema#"):
+        req = """
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    
+            SELECT ?resource ?object
+            WHERE{
+                ?resource xsd:%s ?object.
+            }
+        """
+    elif(uriProperty=="http://erlangen-crm.org/efrbroo/"):
+        req = """
+            PREFIX efrbroo: <http://erlangen-crm.org/efrbroo/>
+    
+            SELECT ?resource ?object
+            WHERE{
+                ?resource efrbroo:%s ?object.
+            }
+        """
+    elif(uriProperty=="http://purl.org/dc/terms/"):
+        req = """
+            PREFIX dcterms: <http://purl.org/dc/terms/>
+    
+            SELECT ?resource ?object
+            WHERE{
+                ?resource dcterms:%s ?object.
+            }
+        """
+
+
+
+    req = req % namespace
     results = graph.query(req)
 
     for result in results:
         print(result)
 
-
-getSubObjSource("U5_had_premiere")
+getSubObjSource("http://erlangen-crm.org/current/P102_has_title",grapheSource)
