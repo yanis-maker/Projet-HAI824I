6747506574bf570896e062f3448fcc0c77b24e46
Simou12 <117384981+Simou12@users.noreply.github.com>
2023-03-10 23:48:19 +0100

    MAJ

--- Outils-Integration/parseRdf.py
+++ Outils-Integration/parseRdf.py
@@ -178,77 +178,16 @@ dic = comparaisonRessources(("http://erlangen-crm.org/current/P3_has_note",),
                             0.5, (m.levenshtein,))
 
 
-def compareURI(value1, value2, measure):
-    m = Measures(3)
-
-    str1 = str(value1)
-    str2 = str(value2)
-    q = prepareQuery(
-        "SELECT ?property ?value WHERE {?subject ?property ?value.}",
-    )
-    result1 = grapheSource.query(q, initBindings={'subject': value1})
-    result2 = grapheCible.query(q, initBindings={'subject': value2})
-    for r in result1:
-        print(r)
-    print("========================")
-    for r1 in result2:
-        print(r1)
-
-    cpt = 0
-    total = 0
-    for p1, v1 in result1:
-        for p2, v2 in result2:
-            if str(p1) == str(p2):
-                print("trouve")
-                cpt += 1
-                # if str(p1) == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type":
-                #     if str(v1) == str(v2):
-                #         total += 1
-                # else:
-                if isinstance(v1, Literal):
-                    total += compareLiteral(v1, v2, measure)
-                if isinstance(v1, URIRef):
-                    total += compareURI(v1, v2, measure)
-                if isinstance(v1, BNode):
-                    total += compareBNode(v1, v2, p1, measure)
-    return total / cpt
-
-
 # compareURI(URIRef("http://data.doremus.org/expression/51cbf519-6243-303f-91f6-f70fe55a92d8"),
 #            URIRef("http://data.doremus.org/expression/c5548a52-1da2-348d-9eb0-311293232d05"), m.levenshtein)
 
 
-def compareBNode(res1, res2, prop, measure):
-    m = Measures(3)
-    q1 = prepareQuery(
-        """SELECT ?property ?value 
-        WHERE {
-            ?subject ?property1 ?subject1. 
-            ?subject1 ?property ?value
-        }"""
-    )
-    res1 = grapheSource.query(q1, initBindings={'subject': res1, 'property1': prop})
-    res2 = grapheCible.query(q1, initBindings={'subject': res2, 'property1': prop})
-    total1 = 0
-    cpt = 0
 
-    for prop1, obj1 in res1:
-        for prop2, obj2 in res2:
-            if (prop1 == prop2):
-                cpt += 1
-                if (isinstance(obj1, Literal)):
-                    total1 = compareLiteral(obj1, obj2, measure)
-                if (isinstance(obj1, URIRef)):
-                    total1 = compareURI(obj1, obj2)
-    return total1 / cpt
 
 
 # compareBNode(URIRef("http://data.doremus.org/event/a7983848-efb9-3757-bdbf-cb068aed4219"),
 #              URIRef("http://data.doremus.org/event/a05cbd75-6238-3c6c-8683-f8c7aa5202a1"),
 #              URIRef("http://erlangen-crm.org/current/P9_consists_of"))
-
-
-
 
 
 def openResultFile(dicRessourceIdentique):
